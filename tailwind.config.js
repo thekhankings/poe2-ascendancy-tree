1dae0f9501026eef01ca4d857d5d574b196ee582
morbi <skit_tre@hotmail.com>
2025-04-02 21:26:52 +0200

    Fixed skill tree not showing on mobile device

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'scale-[1.2]',
+    'scale-[1.3]',
+    'origin-top',
+  ],
   theme: {
     extend: {},
   },
